acc9036534dac665713a654376c6d3e2726dbd48
syhnmzln <113568212+syhnmzln@users.noreply.github.com>
2026-05-12 17:15:01 +0800

    Update _meta.lua

--- ReadingGoals/_meta.lua
+++ ReadingGoals/_meta.lua
@@ -4,5 +4,5 @@ return {
     fullname = _("Reading goal"),
     version = 3,
     description = _([[Set reading goals by % or pages, plus a "Read in X days" book completion goal with adaptive daily targets and compact status labels.]]),
-    author = "ardabooo (forked by myself)",
+    author = "ardabooo (forked by syhnmzln)",
 }
