7df9f6394ca26032c371cfb0639d5559b5839428
syhnmzln <113568212+syhnmzln@users.noreply.github.com>
2026-05-12 14:26:02 +0800

    Update main.lua

--- ReadingGoals/main.lua
+++ ReadingGoals/main.lua
@@ -25,7 +25,7 @@ local ReadingGoal = WidgetContainer:extend{
     reminder_interval = 25,
     reminders_fired = {},
 
-    goal_symbol = "\u{02741}",
+    goal_symbol = "\u{026F1}",
     goal_letter = "G",
 }
 
