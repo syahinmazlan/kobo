4b13fc060c4d6d5402012be00cfea8353e34a497
syhnmzln <113568212+syhnmzln@users.noreply.github.com>
2026-05-25 13:28:22 +0800

    Delete Xray directory

--- Xray/_meta.lua
+++
@@ -1,9 +0,0 @@
--- Plugin metadata
-local _ = require("gettext")
-
-return {
-    name = "xray",
-    fullname = _("X-Ray"),
-    description = _([[Transform your reading experience with AI-powered book analysis, just like Amazon Kindle X-Ray!]]),
-    version = "2.0.0"
-}
